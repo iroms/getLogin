df22f2ffa4692d6bdcb2117f98a7f54d52a5debf
root <root@deb9>
2018-08-05 20:25:35 +1000

    getLogin, 1st variant

---
+++ getLogin.php
@@ -0,0 +1,75 @@
+<?php
+
+    namespace iroms;
+
+    class getLogin
+    {
+
+        public function getLogin($name, $surname, $maxLength = 20)
+        {
+
+            function translit($text)
+            {
+
+                $translitRule = array(
+                    'а' => "a",
+                    'б' => "b",
+                    'в' => "v",
+                    'г' => "g",
+                    'д' => "d",
+                    'е' => "e",
+                    'ё' => "e",
+                    'ж' => "zh",
+                    'з' => "z",
+                    'и' => "i",
+                    'й' => "j",
+                    'к' => "k",
+                    'л' => "l",
+                    'м' => "m",
+                    'н' => "n",
+                    'о' => "o",
+                    'п' => "p",
+                    'р' => "r",
+                    'с' => "s",
+                    'т' => "t",
+                    'у' => "u",
+                    'ф' => "f",
+                    'х' => "h",
+                    'ц' => "tc",
+                    'ч' => "ch",
+                    'ш' => "sh",
+                    'щ' => "shh",
+                    'ъ' => "",
+                    'ы' => "y",
+                    'ь' => "",
+                    'э' => "e",
+                    'ю' => "yu",
+                    'я' => "ya",
+                );
+
+                $text = mb_strtolower($text);                   // нижний регистр
+                $text = strtr($text, $translitRule);            // собственно транслитерация
+                $text = preg_replace('/[^a-z]/', '', $text);    // убирание всех символов, кроме латиницы и кириллицы
+                return $text;
+
+            }
+
+
+            $login = translit($name).'.'.translit($surname);
+
+            if(strlen($login) > $maxLength)
+            {
+                $login = translit($surname);
+                if(strlen($login) > $maxLength)
+                {
+                    $login = substr($login, 0, $maxLength);
+                }
+            }
+
+            return $login;
+
+        }
+
+    }
+
+
